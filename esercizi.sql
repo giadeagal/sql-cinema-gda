a617aaf4b4c6cb9d7d34a36c56749df7628a429a
Gianluca <gl.galassini@gmail.com>
2022-11-16 17:30:27 +0100

    2

--- esercizi.sql
+++ esercizi.sql
@@ -2,3 +2,9 @@
 SELECT *
 FROM sale
 WHERE sale.citta = 'Roma'
+
+/*2 - titolo dei film di "F. Fellini" prodotti post 1960*/
+SELECT *
+FROM film
+WHERE regista = 'F. Fellini'
+AND annoproduzione > 1960
