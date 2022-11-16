d06ad140a9bba3756c989861609070afaea07c90
Gianluca <gl.galassini@gmail.com>
2022-11-16 17:42:23 +0100

    3

--- esercizi.sql
+++ esercizi.sql
@@ -4,7 +4,18 @@ FROM sale
 WHERE sale.citta = 'Roma'
 
 /*2 - titolo dei film di "F. Fellini" prodotti post 1960*/
-SELECT *
+SELECT film.titolo
 FROM film
 WHERE regista = 'F. Fellini'
 AND annoproduzione > 1960
+
+/*3 - titolo dei film di fantascienza giapponesi o francesi prodotti dopo il 1990*/
+
+SELECT titolo, genere, nazionalita, annoproduzione
+FROM film
+WHERE 
+    (film.nazionalita LIKE '_iapponese'
+OR
+    film.nazionalita LIKE '_rancese')
+AND
+    film.annoproduzione > 1990
